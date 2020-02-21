0b4f885e8300845297bcc7f4f447738e6edb547e
Lijo <lijomoncjohn@gmail.com>
2020-02-21 15:43:06 +0530

    Init project

---
+++ app/src/main/java/com/lijo/nasapictures/PictureDetailActivity.java
@@ -0,0 +1,14 @@
+package com.lijo.nasapictures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PictureDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+    }
+}
---
+++ app/src/main/java/com/lijo/nasapictures/PictureGridsActivity.java
@@ -0,0 +1,14 @@
+package com.lijo.nasapictures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PictureGridsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_grids);
+    }
+}
