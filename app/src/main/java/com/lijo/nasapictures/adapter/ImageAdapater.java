5d234867abc1cb5fe19e85d65cb68171b71a4c77
Ernest <youandinews@gmail.com>
2020-02-25 09:49:08 +0100

    Imported the viewHolder package and made some modifications to reduce boiler plate

--- app/src/main/java/com/lijo/nasapictures/adapter/ImageAdapater.java
+++ app/src/main/java/com/lijo/nasapictures/adapter/ImageAdapater.java
@@ -13,10 +13,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.lijo.nasapictures.R;
 import com.lijo.nasapictures.model.ImageResponse;
+import com.lijo.nasapictures.viewholder.MyviewHolder;
 
 import java.util.List;
 
-public class ImageAdapater extends RecyclerView.Adapter<ImageAdapater.MyviewHolder> {
+public class ImageAdapater extends RecyclerView.Adapter<MyviewHolder> {
     Context context;
     List<ImageResponse> dataList;
 
@@ -34,13 +35,13 @@ public class ImageAdapater extends RecyclerView.Adapter<ImageAdapater.MyviewHold
 
     @NonNull
     @Override
-    public ImageAdapater.MyviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.listitem_images,parent,false);
         return new MyviewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ImageAdapater.MyviewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull MyviewHolder holder, final int position) {
         Glide.with(context).load(dataList.get(position).getUrl()).into(holder.image);
         holder.title.setText(dataList.get(position).getTitle());
     }
@@ -50,15 +51,15 @@ public class ImageAdapater extends RecyclerView.Adapter<ImageAdapater.MyviewHold
         return dataList.size();
     }
 
-    public class MyviewHolder extends RecyclerView.ViewHolder {
-       ImageView image;
-       TextView title;
-
-        public MyviewHolder(View itemView) {
-            super(itemView);
-            image = (itemView).findViewById(R.id.photo);
-            title = (itemView).findViewById(R.id.title);
-        }
-    }
+//    public class MyviewHolder extends RecyclerView.ViewHolder {
+//       ImageView image;
+//       TextView title;
+//
+//        public MyviewHolder(View itemView) {
+//            super(itemView);
+//            image = (itemView).findViewById(R.id.photo);
+//            title = (itemView).findViewById(R.id.title);
+//        }
+//    }
 
 }
