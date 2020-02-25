3425cc3f81ccf75000b1901d604705677badb68c
Ernest <youandinews@gmail.com>
2020-02-25 09:37:40 +0100

    Added Constructor

--- app/src/main/java/com/lijo/nasapictures/model/Nasa.java
+++ app/src/main/java/com/lijo/nasapictures/model/Nasa.java
@@ -11,6 +11,17 @@ public class Nasa  {
     private String title;
     private String url;
 
+    public Nasa(String copyright, String date, String explanation, String hdurl, String media_type, String service_version, String title, String url) {
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
+    }
+
     public String getCopyright() {
         return copyright;
     }
